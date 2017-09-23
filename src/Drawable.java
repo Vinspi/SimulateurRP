a6c257ee166cdd237f3b058cc6025dcefbce092a
Vinspi <vinspi13@gmail.com>
2017-09-23 17:25:39 +0200

    ajout interface graphique

---
+++ src/Drawable.java
@@ -0,0 +1,8 @@
+import java.awt.*;
+
+/**
+ * Created by vinspi on 23/09/17.
+ */
+public interface Drawable {
+    public void draw(Graphics g);
+}
---
+++ src/VueRP4.java
@@ -0,0 +1,5 @@
+/**
+ * Created by vinspi on 23/09/17.
+ */
+public class VueRP4 {
+}
